489d7f60c5b674636181263459af7a2daed61f7c
GvYoutube <119984713+Gvyt@users.noreply.github.com>
2025-10-11 08:10:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello Back4apper!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+from threading import Thread
+import backend_code
+Thread(target=backend_code.run).start()
